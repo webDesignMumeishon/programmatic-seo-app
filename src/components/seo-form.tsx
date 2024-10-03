45f402925a2d133f6e8615180834ee3ecd8b2be3
Tomas Macchi <tomas@mutual.app>
2024-10-03 17:41:23 -0300

    style btn cancel

--- src/components/seo-form.tsx
+++ src/components/seo-form.tsx
@@ -38,33 +38,36 @@ export default function SEOForm() {
     }
 
     return (
-        <form onSubmit={onSubmit} className="space-y-4">
-            <div className="space-y-2">
-                <Label htmlFor="city">City</Label>
-                <Input id="city" placeholder="Enter your city" disabled={isLoading} required />
-            </div>
-            <div className="space-y-2">
-                <Label htmlFor="service">Service</Label>
-                <Input id="service" placeholder="Enter your service" disabled={isLoading} required />
-            </div>
-            <div className="space-y-2">
-                <Label htmlFor="website">Website</Label>
-                <Input id="website" placeholder="Enter your website" type="url" disabled={isLoading} required />
-            </div>
-            <div className="space-y-2">
-                <Label htmlFor="companyName">Company Name</Label>
-                <Input id="companyName" placeholder="Enter your company name" disabled={isLoading} required />
-            </div>
-            <Button className="w-full" type="submit" disabled={isLoading}>
-                {isLoading && (
-                    <svg className="mr-2 h-4 w-4 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
-                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
-                    </svg>
-                )}
-                Submit
-            </Button>
-            <Button className="w-full bg-red-600" type="submit" disabled={isLoading} onClick={() => logout()}>
+        <div>
+            <form onSubmit={onSubmit} className="space-y-4">
+                <div className="space-y-2">
+                    <Label htmlFor="city">City</Label>
+                    <Input id="city" placeholder="Enter your city" disabled={isLoading} required />
+                </div>
+                <div className="space-y-2">
+                    <Label htmlFor="service">Service</Label>
+                    <Input id="service" placeholder="Enter your service" disabled={isLoading} required />
+                </div>
+                <div className="space-y-2">
+                    <Label htmlFor="website">Website</Label>
+                    <Input id="website" placeholder="Enter your website" type="url" disabled={isLoading} required />
+                </div>
+                <div className="space-y-2">
+                    <Label htmlFor="companyName">Company Name</Label>
+                    <Input id="companyName" placeholder="Enter your company name" disabled={isLoading} required />
+                </div>
+                <Button className="w-full" type="submit" disabled={isLoading}>
+                    {isLoading && (
+                        <svg className="mr-2 h-4 w-4 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+                        </svg>
+                    )}
+                    Submit
+                </Button>
+
+            </form>
+            <Button className="w-full bg-red-600 mt-4 hover:bg-red-400" type="submit" disabled={isLoading} onClick={() => logout()}>
                 {isLoading && (
                     <svg className="mr-2 h-4 w-4 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
@@ -73,6 +76,6 @@ export default function SEOForm() {
                 )}
                 Cancel
             </Button>
-        </form>
+        </div>
     )
 }
